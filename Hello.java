578ec4af3858820db4682c8eae20926935914624
saron26 <59940863+saron26@users.noreply.github.com>
2020-01-15 18:16:40 -0500

    Add files via upload
    This is the first introductory lab that introduced java.

---
+++ Hello.java
@@ -0,0 +1,20 @@
+/*
+Hello.Java
+
+Saron Worku
+January 15, 2020
+CMSC 255 Section 901
+*/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("<Saron>");
+		System.out.println("<Hello Java>");
+		System.out.println("<This program will introduce you to Java>");
+		System.out.println("<Version 11>");
+		System.out.println("<Course 255 Section 901>");
+		System.out.println();
+		}
+
+}
